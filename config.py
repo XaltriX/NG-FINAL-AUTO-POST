444267c9cc8ec966865ebc6e9d42cae0ea45aa23
XaltriX <129171425+XaltriX@users.noreply.github.com>
2026-01-14 14:25:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Bot Configuration
-BOT_TOKEN = os.getenv('BOT_TOKEN', '7008031473:AAH-ZLZXr5vM2X_0rdMcdSRsfRwG6zzqvYE')
+BOT_TOKEN = os.getenv('BOT_TOKEN', '7008031473:AAEqhmmWZnMGNTgFXGC14degVtTKlPzPysw')
 
 # MongoDB Configuration
 MONGODB_URL = os.getenv('MONGODB_URL', 'mongodb+srv://villainravangaming:mikey_kun_781_@cluster0.fbgs1zz.mongodb.net/?retryWrites=true&w=majority')
@@ -22,5 +22,6 @@ MORE_CHANNELS_LINK = 'https://t.me/Linkz_Wallah/2'
 
 ADMIN_USER_ID = 5706788169  # Set your Telegram user ID here
 OWNER_ID = 5706788169
+
 
 
